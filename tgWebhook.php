2ac6d78b06b18226a914a311943f087144fd8956
Schtil <schtil@schtil.com>
2021-06-28 23:16:19 +0300

    TG Webhook setup

--- tgWebhook.php
+++ tgWebhook.php
@@ -5,39 +5,28 @@ use Telegram\Bot\Api;
 $telegram = new Api(ENV("TG_API_KEY"));
 $result = $telegram -> getWebhookUpdates();
 
-$text = $result["message"]["text"];
-$chat_id = $result["message"]["chat"]["id"];
-$name = $result["message"]["from"]["username"];
-
-file_put_contents("log.txt", json_encode($result));
-
-//if($text){
-//    if ($text == "/start") {
-//        $reply = "Добро пожаловать в бота!";
-//        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-//        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-//    }elseif ($text == "/help") {
-//        $reply = "Информация с помощью.";
-//        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
-//    }elseif ($text == "Картинка") {
-//        $url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
-//        $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
-//    }elseif ($text == "Гифка") {
-//        $url = "https://68.media.tumblr.com/bd08f2aa85a6eb8b7a9f4b07c0807d71/tumblr_ofrc94sG1e1sjmm5ao1_400.gif";
-//        $telegram->sendDocument([ 'chat_id' => $chat_id, 'document' => $url, 'caption' => "Описание." ]);
-//    }elseif ($text == "Последние статьи") {
-//        $html=simplexml_load_file('http://netology.ru/blog/rss.xml');
-//        foreach ($html->channel->item as $item) {
-//            $reply .= "\xE2\x9E\xA1 ".$item->title." (<a href='".$item->link."'>читать</a>)\n";
-//        }
-//        $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
-//    }else{
-//        $reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
-//        $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply ]);
-//    }
-//}else{
-//    $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
-//}
+$botIsAdmin = "нет";
+if(isset($result["message"]["forward_from_chat"])) {
+    $infoChat = json_decode(
+        file_get_contents("https://api.telegram.org/bot".ENV("TG_API_KEY")."/getChat?chat_id=".$result["message"]["forward_from_chat"]["id"]),
+        1
+    );
+    if($infoChat["ok"]) {
+        $botIsAdmin = "да";
+    } else {
+        $botIsAdmin = "нет";
+    }
+    $telegram->sendMessage([
+        'chat_id' => $result["message"]["chat"]["id"],
+        "text" => "Channel Title: ".$result["message"]["forward_from_chat"]["title"]."\n".
+            "Channel ID: ".$result["message"]["forward_from_chat"]["id"]."\n".
+            "Данный бот администратор в этом канале - ". $botIsAdmin,
+    ]);
+}
+$telegram->sendMessage([
+    'chat_id' => $result["message"]["chat"]["id"],
+    "text" => "Привет! Перешли мне сообщение из любого канала и я выведу информацию о нем",
+]);
 
 function ENV($index, $default = NULL)
 {
